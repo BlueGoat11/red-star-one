06459fa1bbeb90f9b363fc992f0b6bfc79453d75
Luke Evanson <156948079+BlueGoat11@users.noreply.github.com>
2024-11-25 08:07:38 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,17 +15,20 @@ function spawnLevel (num: number) {
     }
     badStar = sprites.create(assets.image`badStar`, SpriteKind.Enemy)
     badStar.setPosition(0, 97)
-    badStar.y += 390
-    info.startCountdown(3)
-    pause(3000)
-    badStar.follow(forest, randint(70, 90))
+    badStar.y += 360
+    timer.after(2000, function () {
+        badStar.follow(forest, randint(70, 90))
+    })
 }
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     temp = 0
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    forest.vy += -200
-    pause(500)
+    if (gameStart == 1) {
+        forest.vy += -200
+        music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        pause(100)
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -146,7 +149,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 info.onLifeZero(function () {
-    sprites.destroy(forest, effects.disintegrate, 500)
+    sprites.destroy(forest, effects.disintegrate, 1000)
     pause(1000)
     game.gameOver(false)
 })
@@ -160,11 +163,15 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 let temp = 0
 let badStar: Sprite = null
 let forest: Sprite = null
+let gameStart = 0
+gameStart = 0
 scene.setBackgroundImage(assets.image`start`)
+game.showLongText("An odd voice says that this game is made by Luke.", DialogLayout.Bottom)
 music.play(music.stringPlayable("C5 A B G A F G E ", 70), music.PlaybackMode.UntilDone)
 scene.setBackgroundImage(assets.image`normalBG`)
 game.showLongText("Your name is Forest, and you have been training your whole life to complete the ancient obstacle course of GOAT EATING SANDWICH.", DialogLayout.Full)
 game.showLongText("Suddenly, an (apparently) evil red star starts to follow you. Your only guess is if it touches you, you're cooked. You must get as far as you can through the ancient ruins of GOAT EATING SANDWICH.", DialogLayout.Full)
+gameStart = 1
 forest = sprites.create(assets.image`idle`, SpriteKind.Player)
 scene.cameraFollowSprite(forest)
 tiles.setCurrentTilemap(tilemap`one`)
@@ -173,9 +180,9 @@ controller.moveSprite(forest, 100, 0)
 spawnLevel(1)
 info.setLife(5)
 forever(function () {
-    music.play(music.stringPlayable("E B C5 A B G A F ", 95), music.PlaybackMode.UntilDone)
-})
-forever(function () {
     forest.vy += temp
     temp += 1
+})
+forever(function () {
+    music.play(music.stringPlayable("E B C5 A B G A F ", 95), music.PlaybackMode.UntilDone)
 })
